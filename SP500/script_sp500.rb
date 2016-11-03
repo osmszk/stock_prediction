5656c0420a10a0c3d50e36e9434e432f72650406
osuzukix <osuzuki@plegineer.com>
2016-11-03 21:14:18 +0900

    result of vix26

--- SP500/script_sp500.rb
+++ SP500/script_sp500.rb
@@ -34,14 +34,16 @@ end
 base = new_data[0]["Close"].to_f
 
 File.open("output_sp500.csv", 'w') do |file|
-  file.write("Date,Value,Ratio,HALF_YEAR,VIX\n")
+  file.write("Date,Value,Ratio,HALF_YEAR,VIX_STS\n")
   i = 0
   temp_half_year = ""
   temp_ratio = 100.0
+  temp_vix_sts = 100.0
+  temp_vix = 20.0
   new_data.each do |data|
     value = data["Close"].to_f
     date_str = data["Date"]
-    vix = data["Vix"].to_f
+    vix_value = data["Vix"].to_f
     ratio = (value - base) / base * 100.0 + 100.0
 
     if i > 0 && should_buy_priod(date_str)
@@ -49,18 +51,33 @@ File.open("output_sp500.csv", 'w') do |file|
       prev_half_year = temp_half_year.to_f
       half_year = (ratio - prev_ratio) / prev_ratio * prev_half_year + prev_half_year
       # puts "should buy! #{date_str} "
+
+      prev_vix = temp_vix
+      if vix_value > 26.0
+        #not hold
+        vix_sts = temp_vix_sts.to_f
+      else
+        #hold
+        prev_vix_sts = temp_vix_sts.to_f
+        vix_sts = (ratio - prev_ratio) / prev_ratio * prev_vix_sts + prev_vix_sts
+      end
+
     elsif i > 0 && !should_buy_priod(date_str)
       prev_ratio = temp_ratio
       prev_half_year = temp_half_year.to_f
       half_year = temp_half_year.to_f
       # puts "should NOT buy! #{date_str} "
+
+      vix_sts = temp_vix_sts.to_f
     else
       half_year = "100.0"
+      vix_sts = "100.0"
     end
 
-    intro_msg = "#{date_str},#{value},#{ratio},#{half_year},#{vix}\n"
+    intro_msg = "#{date_str},#{value},#{ratio},#{half_year},#{vix_sts}\n"
     temp_half_year = half_year
     temp_ratio = ratio
+    temp_vix_sts = vix_sts
     puts intro_msg
     file.write(intro_msg)
     i = i + 1
