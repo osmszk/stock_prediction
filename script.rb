a2417f6b815825a55f7c769173aa314708db7bea
osuzukix <osuzuki@plegineer.com>
2016-11-03 14:44:37 +0900

    fix

--- script.rb
+++ script.rb
@@ -1,16 +1,23 @@
 require 'csv'
 
+# date_slash:string
+def convert_date(date_slash)
+  date_array = date_slash.split("/")
+  if date_array.length == 3
+    return "20#{date_array[2]}/#{date_array[0]}/#{date_array[1]}"
+  else
+    return ""
+  end
+end
+
 csv_data = CSV.read('test.csv', headers: true)
 puts "start..."
 
-# Date, Open, High, Low, Close
-# 11/02/16, 1376.68, 1377.61, 1363.02, 1368.44
-# 11/01/16, 1391.97, 1394.92, 1384.45, 1393.19
-# test.csv
 File.open("output.csv", 'w') do |file|
   file.write("Date,Close\n")
   csv_data.reverse_each do |data|
-    intro_msg = "#{data["Date"]},#{data[" Close"]}\n"
+    date = convert_date("#{data["Date"]}")
+    intro_msg = "#{date},#{data[" Close"]}\n"
     puts intro_msg
     file.write(intro_msg)
   end
