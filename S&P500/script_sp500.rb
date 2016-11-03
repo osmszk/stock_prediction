17e7b2a65d8c302d091485932346ec988f502dce
osuzukix <osuzuki@plegineer.com>
2016-11-03 18:02:45 +0900

    fix sp500

--- S&P500/script_sp500.rb
+++ S&P500/script_sp500.rb
@@ -1,4 +1,12 @@
 require 'csv'
+require 'date'
+
+
+def should_buy_priod(date_str)
+  date = DateTime.parse(date_str)
+  month = date.month.to_i
+  return month >= 11 || month <= 4
+end
 
 csv_data = CSV.read('rawdata_sp500.csv', headers: true)
 puts "start..."
@@ -15,15 +23,35 @@ end
 base = new_data[0]["Close"].to_f
 
 File.open("output_sp500.csv", 'w') do |file|
-  file.write("Date,Index,Ratio\n")
-  index = 0
+  file.write("Date,Value,Ratio,HALF_YEAR\n")
+  i = 0
+  temp_half_year = ""
+  temp_ratio = 100.0
   new_data.each do |data|
     value = data["Close"].to_f
-    ratio = (value - base) / base * 100.0 + 100.0 
-    intro_msg = "#{data["Date"]},#{value},#{ratio}\n"
+    date_str = data["Date"]
+    ratio = (value - base) / base * 100.0 + 100.0
+
+    if i > 0 && should_buy_priod(date_str)
+      prev_ratio = temp_ratio
+      prev_half_year = temp_half_year.to_f
+      half_year = (ratio - prev_ratio) / prev_ratio * prev_half_year + prev_half_year
+      # puts "should buy! #{date_str} "
+    elsif i > 0 && !should_buy_priod(date_str)
+      prev_ratio = temp_ratio
+      prev_half_year = temp_half_year.to_f
+      half_year = temp_half_year.to_f
+      # puts "should NOT buy! #{date_str} "
+    else
+      half_year = "100.0"
+    end
+
+    intro_msg = "#{date_str},#{value},#{ratio},#{half_year}\n"
+    temp_half_year = half_year
+    temp_ratio = ratio
     puts intro_msg
     file.write(intro_msg)
-    index = index + 1
+    i = i + 1
   end
 end
 
