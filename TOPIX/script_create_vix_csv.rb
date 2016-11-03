1bcc1b05e5a28601c82bed4ec8babf4b39e93b78
osuzukix <osuzuki@plegineer.com>
2016-11-03 23:28:28 +0900

    insert to vix to topix data

--- TOPIX/script_create_vix_csv.rb
+++ TOPIX/script_create_vix_csv.rb
@@ -1,6 +1,16 @@
 require 'csv'
-require 'date'
 
+# date_slash:string
+# is_twenty:boolean
+def convert_date(date_slash,is_twenty)
+  date_array = date_slash.split("/")
+  if date_array.length == 3
+    header = is_twenty ? "20" : "19"
+    return "#{header}#{date_array[2]}/#{date_array[0]}/#{date_array[1]}"
+  else
+    return ""
+  end
+end
 
 def should_buy_priod(date_str)
   date = DateTime.parse(date_str)
@@ -8,34 +18,62 @@ def should_buy_priod(date_str)
   return month >= 11 || month <= 4
 end
 
-csv_data = CSV.read('rawdata_sp500.csv', headers: true)
+csv_data = CSV.read('rawdata_topix.csv', headers: true)
 csv_vix_data = CSV.read('rawdata_vix.csv', headers: true)
 
 puts "start..."
 
 new_data = []
-# new_csv_data.push("Date,Close\n")
+index = 0
 csv_data.reverse_each do |data|
-  date_str = data["Date"]
-  close_str = data["Close"]
+  is_twenty = index <= 983 ? false : true
+  date_str = convert_date("#{data["Date"]}", is_twenty)
+  close_str = data[" Close"]
   hash = {"Date" => date_str,"Close" => close_str}
   new_data.push(hash)
+  index = index + 1
 end
 
-k = 0
+puts "insert vix data..."
+
 csv_vix_data.reverse_each do |data|
   close_str = data["Close"]
-  hash = new_data[k]
-  hash["Vix"] = close_str
-  new_data[k] = hash
-  k = k + 1
+  date_str = data["Date"]
+  vix_date = DateTime.parse(date_str)
+  new_data.each_with_index do |hash,i|
+    n_date = DateTime.parse(hash["Date"])
+    if (vix_date.year.to_i == n_date.year.to_i &&
+      vix_date.month.to_i == n_date.month.to_i &&
+      vix_date.day.to_i == n_date.day.to_i )
+      new_hash = hash
+      new_hash["Vix"] = close_str
+      new_data[i] = new_hash
+      next
+    end
+  end
 end
 
+puts "writing..."
+
+File.open("output_topix_with_vix.csv", 'w') do |file|
+  file.write("Date,Value,Vix\n")
+  new_data.each do |data|
+    value = data["Close"].to_f
+    date_str = data["Date"]
+    vix_value = data["Vix"].to_f
+
+    intro_msg = "#{date_str},#{value},#{vix_value}\n"
+    puts intro_msg
+    file.write(intro_msg)
+  end
+end
+
+=begin
 base = new_data[0]["Close"].to_f
 
-File.open("output_sp500.csv", 'w') do |file|
+File.open("output_topix.csv", 'w') do |file|
   file.write("Date,Value,Ratio,HALF_YEAR,VIX_STS,VIX_VAL\n")
-  # S&P500value	S&P500	半年投資(4月〜翌11月)	半年投資+VIX26	VIX
+
   i = 0
   temp_half_year = ""
   temp_ratio = 100.0
@@ -86,5 +124,6 @@ File.open("output_sp500.csv", 'w') do |file|
     i = i + 1
   end
 end
+=end
 
 puts "complete!"
